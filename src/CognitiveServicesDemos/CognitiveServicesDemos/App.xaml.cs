6a2ac97fd3320e84ccc55ba7d7cf62edf6fb3578
Jordan Knight <jakkaj@outlook.com>
2016-11-07 23:10:25 +1100

    Started the joint demo app

---
+++ src/CognitiveServicesDemos/CognitiveServicesDemos/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Autofac;
+
+namespace CognitiveServicesDemos
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static IContainer Container { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var c = new ContainerBuilder();
+            c.RegisterAssemblyTypes(typeof(App).Assembly)
+                .Where(_ => _.FullName.EndsWith("View") || _.FullName.EndsWith("ViewModel"))
+                .AsSelf();
+
+            Container = c.Build();
+        }
+    }
+}
---
+++ src/CognitiveServicesDemos/CognitiveServicesDemos/Views/HomeView.model.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using CognitiveServicesDemos.Model.App;
+
+namespace CognitiveServicesDemos.Views
+{
+    public class HomeViewModel : ViewModel
+    {
+        public List<HomeViewItem> HomeViews { get; set; }
+        public HomeViewModel()
+        {
+            HomeViews = new List<HomeViewItem>()
+            {
+                new HomeViewItem
+                {
+                    Title = "Vision", 
+                    SubText = "From faces to feelings, allow your apps to understand images and video", 
+                    Image = "/CognitiveServicesDemos;component/Assets/Picture1.png"
+                },
+                 new HomeViewItem
+                {
+                    Title = "Speech",
+                    SubText = "Hear and speak to your users by filtering noise, identifying speakers, and understanding intent",
+                    Image = "/CognitiveServicesDemos;component/Assets/Picture2.png"
+                },
+                  new HomeViewItem
+                {
+                    Title = "Vision",
+                    SubText = "From faces to feelings, allow your apps to understand images and video",
+                    Image = "/CognitiveServicesDemos;component/Assets/Picture3.png"
+                }
+            };
+        }
+    }
+
+    public class HomeViewItem
+    {
+        public string Title { get; set; }
+        public string SubText { get; set; }
+        public string Image { get; set; }
+        public Page Page { get; set; }
+    }
+}
